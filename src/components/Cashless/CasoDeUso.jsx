db8c36a45aa513618bdd29d0fbbadcb6b149fc53
Joshwawo <masacota_masacota@hotmail.com>
2022-07-26 14:20:53 -0700

    cashless semidone fix 2.0

--- src/components/Cashless/CasoDeUso.jsx
+++ src/components/Cashless/CasoDeUso.jsx
@@ -1,11 +1,10 @@
-import  casodeuso  from "../../img/Cashless/CasoDeUso.webp";
-
+import casoDeUso from '../../img/Cashless/CasoDeUso.webp'
 const CasoDeUso = () => {
   return (
     <div className="w-full">
       <p className=" monserrat-bold text-center text-2xl">Caso de usos</p>
       <div className="">
-        <img src={casodeuso} alt="Caso de uso" className="w-full xl:w-[90%] mx-auto" />
+        <img src={casoDeUso} alt="Caso de uso" className="w-full xl:w-[90%] mx-auto" />
       </div>
       <div className="items md:flex justify-around gap-2 text-center monserrat-regular">
       </div>
