c9ecb7c886049393fbe9b04ff604933426775179
Joshwawo <masacota_masacota@hotmail.com>
2022-10-27 14:50:02 -0700

    add: url redirects in home

--- src/components/Cashless/Banner.jsx
+++ src/components/Cashless/Banner.jsx
@@ -4,6 +4,7 @@ import login from "../../img/Cashless/Capa_24.webp";
 import verSaldo from "../../img/Cashless/Capa-25.webp";
 import ios from "../../img/PYS/Capa_13.webp";
 import android from "../../img/PYS/Capa_14.webp";
+import iglogo from "../../img/PYS/iglogowebp.webp";
 
 
 // import bg from "../../img/Cashless/Cashless-fondo.webp";
@@ -21,23 +22,26 @@ const Banner = () => {
               src={logo}
               alt="Logo cashless"
               className=" mb-5 mx-auto xl:m-5 w-42"
-              
             />
             <p className=" text-white text-lg mb-7 xl:text-xl">
               Con Cashless incrementa tus beneficios y facilita la gestión de
               pagos gracias a nuestro sistema de pagos cashless.
             </p>
-           <div className="flex gap-5 flex-row">
-             <a href="https://apps.apple.com/mx/app/cashless-estadios/id1588139450">
-             <img src={ios} alt="Download Ios/Android" className="mb-10"  />
-             </a>
-             <a href="https://play.google.com/store/apps/details?id=com.bs.pv">
-             <img src={android} alt="Download Ios/Android" className="mb-10"  />
-
-             </a>
-           </div>
-            
-
+            <div className="flex gap-5 flex-row">
+              <a href="https://apps.apple.com/mx/app/cashless-estadios/id1588139450">
+                <img src={ios} alt="Download Ios/Android" className="mb-10" />
+              </a>
+              <a href="https://play.google.com/store/apps/details?id=com.bs.pv">
+                <img
+                  src={android}
+                  alt="Download Ios/Android"
+                  className="mb-10"
+                />
+              </a>
+              <a href="https://www.instagram.com/cashlessestadios">
+                <img src={iglogo} alt="Ig logo" className="w-14" />
+              </a>
+            </div>
           </div>
         </div>
         <div className="izquierda md:w-1/2 md:flex  ">
@@ -47,7 +51,12 @@ const Banner = () => {
             className=" hidden  xl:w-[50%] xl:h-[50%] xl:block relative xl:top-[20rem] xl:left-24"
             loading="lazy"
           />
-          <img src={login} alt="Login app " className=" hidden md:block  xl:w-[20rem] xl:h-[40rem]" loading="lazy" />
+          <img
+            src={login}
+            alt="Login app "
+            className=" hidden md:block  xl:w-[20rem] xl:h-[40rem]"
+            loading="lazy"
+          />
         </div>
       </div>
     </section>
--- src/components/Home/Carrousel.jsx
+++ src/components/Home/Carrousel.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import codikash from "../../img/codikash.webp";
 import cashless from "../../img/cashless.webp";
 
-import bgcashless from "../../img/Home/cashless.webp";
+import bgcashlessdeprecate from "../../img/Home/cashless.webp";
 import bgcodikash from "../../img/Home/codikash.webp";
+import bgcashless from "../../img/Home/bgCashlessig.png";
 import controlA from "../../img/Home/controlacceso.webp";
 // import tablero from '../../img/Tableros/Portada.webp'
-import tablero from '../../img/Tableros/fondowebcarrousel.webp'
+import tablero from "../../img/Tableros/fondowebcarrousel.webp";
 
 //asdasd asdasdadadsadsadsasdasdasdasdasd
 
@@ -20,8 +21,9 @@ const Carrusel = () => {
           <img
             src={bgcodikash}
             className="m-auto h-[11rem] md:h-auto "
-            alt="cashless app"
+            alt="Codikash app"
           />
+
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" className=" ">
               <span className="text-white">❮</span>
@@ -29,6 +31,19 @@ const Carrusel = () => {
             <p className=" text-white md:text-2xl  absolute top-[4.3rem] md:top-20 xl:top-40 xl:left-20">
               Codikash
             </p>
+            {/* <p className=" text-red-500 relative left-[45rem] top-[19rem] bg-blue-500 p-2">HELLO THERE</p> */}
+            <a
+              className=" relative  left-[55.5rem] top-[19rem] px-20 py-2"
+              href="https://play.google.com/store/apps/details?id=com.promopixeles.codikash_app"
+            >
+              .
+            </a>
+            <a
+              className=" relative left-[34rem] top-[19rem] px-20"
+              href="https://apps.apple.com/mx/app/codikash-clientes/id1577211896"
+            >
+              .
+            </a>
             <NavLink to="/codikash">
               <button
                 className="black text-white font-bold  border w-20 py-[.2rem] md:py-2 hover:bg-white hover:text-black 
@@ -44,7 +59,7 @@ const Carrusel = () => {
           </div>
         </div>
 
-        <div id="slide2" className="carousel-item relative w-full pt-20 ">
+        <div id="slide2" className="carousel-item relative w-full pt-2 ">
           <img
             src={bgcashless}
             className="m-auto h-[11rem] md:h-auto "
@@ -57,9 +72,29 @@ const Carrusel = () => {
             <p className=" text-white md:text-2xl  absolute top-[4.3rem] md:top-20 xl:top-40 xl:left-20">
               Cashless
             </p>
+            <a
+              className="ANDROID  relative  right-[5rem] top-[13.4rem] px-20 py-2"
+              href="https://play.google.com/store/apps/details?id=com.bs.pv"
+            >
+              .
+            </a>
+            <a
+              className="IOS relative  right-[20rem] top-[13.4rem]  px-20"
+              href="https://apps.apple.com/mx/app/cashless-estadios/id1588139450"
+            >
+              .
+            </a>
+            <a
+              className="IG relative  right-[37rem] top-[13.4rem]  px-7"
+              href="https://www.instagram.com/cashlessestadios/"
+            >
+              .
+            </a>
             <NavLink to="/cashless">
-              <button className="black text-white font-bold py-[.2rem] border md:py-2 w-20 hover:bg-white hover:text-black 
-              absolute  left-[-.1rem] top-[5.9rem]  md:top-[7rem] xl:top-48 xl:left-20">
+              <button
+                className="black text-white font-bold py-[.2rem] border md:py-2 w-20 hover:bg-white hover:text-black 
+              absolute  left-[-.1rem] top-[5.9rem]  md:top-[7rem] xl:top-48 xl:left-20"
+              >
                 Ver
               </button>
             </NavLink>
@@ -80,11 +115,13 @@ const Carrusel = () => {
               <span className="text-white">❮</span>
             </a>
             <p className=" text-white md:text-2xl  absolute top-[4.3rem] md:top-20 xl:top-40 xl:left-20">
-              Control de accesos 
+              Control de accesos
             </p>
             <NavLink to="/gestionacceso">
-              <button className="black text-white font-bold py-[.2rem]   border md:py-2   w-20 hover:bg-white hover:text-black 
-              absolute  left-[-.1rem] top-[5.9rem]  md:top-[7rem] xl:top-48 xl:left-20">
+              <button
+                className="black text-white font-bold py-[.2rem]   border md:py-2   w-20 hover:bg-white hover:text-black 
+              absolute  left-[-.1rem] top-[5.9rem]  md:top-[7rem] xl:top-48 xl:left-20"
+              >
                 Ver
               </button>
             </NavLink>
@@ -105,11 +142,13 @@ const Carrusel = () => {
               <span className="text-white">❮</span>
             </a>
             <p className=" text-white md:text-2xl  absolute top-[4.3rem] md:top-20 xl:top-40 xl:left-20">
-             Tableros inteligencia de negocios
+              Tableros inteligencia de negocios
             </p>
             <NavLink to="/tableros">
-              <button className="black text-white font-bold py-[.2rem]   border md:py-2   w-20 hover:bg-white hover:text-black 
-              absolute  left-[-.1rem] top-[5.9rem]  md:top-[7rem] xl:top-48 xl:left-20">
+              <button
+                className="black text-white font-bold py-[.2rem]   border md:py-2   w-20 hover:bg-white hover:text-black 
+              absolute  left-[-.1rem] top-[5.9rem]  md:top-[7rem] xl:top-48 xl:left-20"
+              >
                 Ver
               </button>
             </NavLink>
@@ -119,7 +158,6 @@ const Carrusel = () => {
             </a>
           </div>
         </div>
-        
       </div>
     </div>
   );
